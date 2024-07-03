5fd860dd0ea1d1f0d5ed85a2ca6c8914f153aab4
Li Jinliang <necerlixin@outlook.com>
2024-07-03 10:29:43 +0800

    症状分类bert+句法树模型完成--李锦亮

--- model.py
+++ model.py
@@ -2,6 +2,8 @@ import torch
 import torch.nn as nn
 from transformers import BertModel
 from torchcrf import CRF
+from torch_geometric.data import Data, DataLoader, Batch
+from torch_geometric.nn import GCNConv, GATConv
 class NerModelLSTM(nn.Module):
     def __init__(self, num_embeddings, embedding_dim,num_labels,hidden_size):
         super().__init__()
@@ -80,7 +82,40 @@ class ClsModelBertBase(nn.Module):
         embedding = torch.tanh(embedding * self.symptom_matrix.unsqueeze(dim=0)) # [b,1,e] * [1,m,e] = [b,m,e]
         output = self.cls(embedding) # [batch,num_labels]
         return output
-    
+
+class ClsModelBertSyntaxTree(nn.Module):
+    def __init__(self,bert_model,hidden_size=768,num_symptoms=331,num_labels=3,gat_heads=4) -> None:
+        super().__init__()
+        self.bert_model = bert_model
+        self.hidden_size = hidden_size
+        self.gat_heads = gat_heads
+        self.trans = nn.Linear(hidden_size*gat_heads,hidden_size)
+        self.cls = nn.Linear(hidden_size,num_labels)
+        self.symptom_matrix = nn.Parameter(torch.randn(num_symptoms,hidden_size))
+        self.gat1 = GATConv(hidden_size,hidden_size,heads=gat_heads)
+
+    def forward(self, input_ids, attention_mask, edge_index_list:list):
+        embedding = self.bert_model(input_ids=input_ids,
+                            attention_mask=attention_mask).last_hidden_state
+        batch_size = len(edge_index_list)
+        graph_data_list = []
+        for i in range(batch_size):
+            edge_index = torch.tensor(edge_index_list[i]).to(input_ids.device).T
+            data = Data(x=embedding[i],edge_index=edge_index)
+            graph_data_list.append(data)
+        batch_data = Batch.from_data_list(graph_data_list)
+        split_sizes = batch_data.ptr[1:] - batch_data.ptr[:-1]
+        embedding = self.gat1(batch_data.x,batch_data.edge_index)
+        
+        embedding = embedding.split(split_sizes.tolist()) # [b,l,e]
+        embedding = torch.stack(embedding,dim=0)
+        embedding = self.trans(embedding)
+        embedding = embedding[:,0,:]#[batch,emb] 使用cls的 embedding 代表作为句子的 embedding
+        embedding = embedding.unsqueeze(dim=1) # [b,1,e]
+        embedding = torch.tanh(embedding * self.symptom_matrix.unsqueeze(dim=0)) # [b,1,e] * [1,m,e] = [b,m,e]
+        output = self.cls(embedding) # [batch,num_labels]
+        return output
+
 
         
         
